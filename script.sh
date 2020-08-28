fce6c4905b01fca53e1e61dd6e5f252584f0577a
sampalm <samuelpalmeira@outlook.com>
2020-08-28 15:57:22 -0300

    fix path

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 crontab -l > mon_cron
 cat >> mon_cron << EOF
-*/5 * * * * /usr/bin/sudo /home/ubuntu/cron.sh >> /var/www/html/logs/cron.log
+*/5 * * * * /usr/bin/sudo /usr/local/bin/aws-ec2-nginx-cf/cron.sh >> /var/www/html/logs/cron.log
 EOF
 crontab < mon_cron
 rm -f mon_cron
