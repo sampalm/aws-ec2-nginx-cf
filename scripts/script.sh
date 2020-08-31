4ac093a6e4693750da734a1aa8db455e3bfccdc9
sampalm <samuelpalmeira@outlook.com>
2020-08-31 14:57:24 -0300

    adds scripts new path

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,6 @@
 crontab -l > mon_cron
 cat >> mon_cron << EOF
-* * * * * /usr/bin/sudo /usr/local/bin/aws-ec2-nginx-cf/cron.sh >> /var/www/html/logs/cron.log
+* * * * * /usr/bin/sudo sh /usr/local/bin/aws-ec2-nginx-cf/scripts/cron.sh >> /var/www/html/logs/cron.log 2>&1
 EOF
 crontab < mon_cron
 rm -f mon_cron
